986a0cd07c25542932b0b88ce8654f3a29f59c1f
Maciek Rogozinski <maciek@slashdev.team>
2017-10-18 15:16:07 +0200

    Fixed random pass key generation

--- src/sensors/randompasskey.cpp
+++ src/sensors/randompasskey.cpp
@@ -5,11 +5,6 @@ PassKey randomPassKey()
 {
     PassKey randKey;
 
-    uint32_t rdm = 0;
-
-    rdm = time(nullptr);
-    srand(rdm);
-
     for(size_t i = 0; i < PASS_KEY_LEN; ++i)
     {
         randKey.data()[i] = rand() & 0xFF;
